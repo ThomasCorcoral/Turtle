b0348590c87f725d9bd079acf74e25ac4d521679
ThomasCorcoral <thomas.corcoral@edu.univ-fcomte.fr>
2021-03-05 16:32:49 +0100

    update

--- turtle-ast.h
+++ turtle-ast.h
@@ -71,6 +71,8 @@ struct ast_node *make_expr_value(double value);
 struct ast_node *make_cmd_forward(struct ast_node *expr);
 struct ast_node *make_cmd_backward(struct ast_node *expr);
 struct ast_node *make_cmd_position(struct ast_node *expr);
+struct ast_node *make_cmd_up(struct ast_node *expr);
+struct ast_node *make_cmd_down(struct ast_node *expr);
 struct ast_node *make_cmd_right(struct ast_node *expr);
 struct ast_node *make_cmd_left(struct ast_node *expr);
 struct ast_node *make_cmd_heading(struct ast_node *expr);
